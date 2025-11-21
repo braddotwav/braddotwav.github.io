0b552d8bd40ccda170013a557adc898fa2b56217
Brad <90891134+braddotwav@users.noreply.github.com>
2025-11-21 16:19:24 +0000

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,29 +1,31 @@
-import { z, defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { z, defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
 
 const profiles = defineCollection({
-    loader: glob({ pattern: '**/*.md', base: './src/data/profiles' }),
-    schema: ({ image }) => z.object({
-        name: z.string(),
-        avatar: image(),
-        hero: image(),
-        description: z.string(),
-        tools: z.array(z.string()),
-        socials: z.array(z.string())
-    })
-})
+  loader: glob({ pattern: "**/*.md", base: "./src/data/profiles" }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+      hero: image(),
+      description: z.string(),
+      tools: z.array(z.string()),
+      socials: z.array(z.string()),
+    }),
+});
 
 const projects = defineCollection({
-    loader: glob({ pattern: '**/*.md', base: './src/data/projects' }),
-    schema: ({ image }) => z.object({
-        name: z.string(),
-        description: z.string(),
-        date: z.date(),
-        category: z.enum(['software', 'audio']),
-        image: image(),
-        tools: z.array(z.string()),
-        links: z.array(z.string().url())
-    })
+  loader: glob({ pattern: "**/*.md", base: "./src/data/projects" }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      description: z.string(),
+      date: z.date(),
+      category: z.enum(["software", "audio", "game"]),
+      image: image(),
+      tools: z.array(z.string()),
+      links: z.array(z.string().url()),
+    }),
 });
 
 export const collections = { profiles, projects };
