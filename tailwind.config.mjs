1901e403994aa8dc5d9d36ebad78cb446b832b3e
Brad <90891134+braddotwav@users.noreply.github.com>
2025-12-03 16:37:14 +0000

    Misc and formatting

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,33 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily:{
-				'sans': 'Onest',
-				'mono': 'Jetbrains'
-			},
-			colors:{
-				'primary': {
-					100: '#e7e7e7',
-					200: '#d0d0d0',
-					300: '#b8b8b8',
-					400: '#a0a0a0',
-					500: '#898989',
-					600: '#717171',
-					700: '#595959',
-					800: '#414141',
-					900: '#2a2a2a',
-					1000: '#121212'
-				},
-				'accent': {
-					100: '#1a2616',
-					200: '#4faa2e',
-					300: '#59c033',
-					400: '#63d539'
-				}
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Onest",
+        mono: "Jetbrains Mono",
+      },
+      colors: {
+        primary: {
+          100: "#e7e7e7",
+          200: "#d0d0d0",
+          300: "#b8b8b8",
+          400: "#a0a0a0",
+          500: "#898989",
+          600: "#717171",
+          700: "#595959",
+          800: "#414141",
+          900: "#2a2a2a",
+          1000: "#121212",
+        },
+        accent: {
+          100: "#1a2616",
+          200: "#4faa2e",
+          300: "#59c033",
+          400: "#63d539",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
